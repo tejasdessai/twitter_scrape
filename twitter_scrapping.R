d46d34e3bf40d6c01612a42adcf97cd639a85d6f
TejasRD <tejas.connect@gmail.com>
2017-05-02 23:23:52 -0400

    Update twitter_scrapping.R

--- twitter_scrapping.R
+++ twitter_scrapping.R
@@ -21,10 +21,10 @@ library(scales)
 library(broom)
 library(ggplot2)
 
-consumerKey = 	'0ppSis0abzyBBEKq4Wle2mkQQ'
-consumerSecret = 'XdFhYnYMyNPMe8SUtpojrCzqKIR5bg10x5t1HzQjMcCcZOL0CI'
-accessToken = '146064028-H0771CDu3fhiOiF8xOvD0R3nwp03YzxC2kBr8fVc'
-accessSecret = 'WStigiau31eiNXHriy9AC6cKttKPHxxo6pcAVSWJ6eOvW'
+consumerKey = 	'#####################'
+consumerSecret = '################################'
+accessToken = '#################################'
+accessSecret = '###############################################'
 options(httr_oauth_cache=TRUE)
 
 setup_twitter_oauth(consumer_key = consumerKey, consumer_secret = consumerSecret,
